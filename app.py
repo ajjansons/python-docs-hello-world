8fc00a6c3bd9238fccbb35b326dcf037adc1ca58
ajjansons <73335793+ajjansons@users.noreply.github.com>
2022-08-26 11:29:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, World! wohooooooo this is my first azure app deployed \n KPMG \n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG\n KPMG"
 
